7c6c086b4de3caad996bc61ffd26dcc0f30bed19
sergio andres <rodascruzsergio@gmail.com>
2022-12-15 15:16:31 -0400

    modify urls in projects

--- src/data/dataProjects.js
+++ src/data/dataProjects.js
@@ -10,7 +10,7 @@ const dataProjects = [
     ]
   },
   {
-    url: 'https://tourbolsergioski9.herokuapp.com/',
+    url: 'https://tourbol.onrender.com/',
     title: 'Tourbol',
     description: 'Web page of an informative but dynamic type, since it is focused on the user and client, in regards to tourism. The objective of this project is that both the locals of the places you want to visit can publish their artisan shops or any type of services that they provide in the place, and it also gives detailed information to the tourist and so they are prepared when they want to go to somewhere.',
     imgPath: 'https://res.cloudinary.com/dgzbf6kp2/image/upload/v1669239944/development/tourbol_svyc8k.png',
@@ -20,7 +20,7 @@ const dataProjects = [
     ]
   },
   {
-    url: 'https://tag-places-sergioski9.herokuapp.com/',
+    url: 'https://tag-places-bolivia.onrender.com/',
     title: 'Tourism and Guides',
     description: 'Dynamic web page, focused mainly on the user (guide) and the client (tourist), where you can book tours and hire local guides to show you the best places, here the guide offers his guide services and the page is in charge of Schedule an appointment and coordinate the tour to facilitate and assure the tourist that when they visit a place they do not miss anything and also know the best areas to observe the landscapes.',
     imgPath: 'https://res.cloudinary.com/dgzbf6kp2/image/upload/v1669239944/development/tag_livq8i.png',
